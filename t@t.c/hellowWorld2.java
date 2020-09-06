bcd389c3ba92eea67036637353b4959c3d717035
Tejaswini Rajkumar <tejr1601@gmail.com>
2020-09-05 21:30:44 -0400

    Delete hellowWorld2.java

--- t@t.c/hellowWorld2.java
+++
@@ -1,6 +0,0 @@
-public class hellowWorld2 {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World!! Again");
-   }
-}
